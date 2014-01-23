7a02243071a09812afe737f6352fa76c3d9f12fa
Andy Heaton <heatonandy@gmail.com>
2014-01-23 16:02:26 -0700

    added sql connection, and started a dinky admin page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mysql = require('mysql');
 
 var app = express();
 
@@ -34,6 +35,24 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/users/:id', user.getUser);
 app.post('/users/:id', user.postUser);
+
+app.get('/admin', routes.admin);
+
+console.log('Attempting to connect to the MySQL database...');
+
+// database connection
+var connection = mysql.createConnection({
+	host : 'searchandrescue.db.6706839.hostedresource.com',
+	user : 'searchandrescue',
+	password : 'ISRock5!',
+	database : 'searchandrescue'
+});
+
+console.log('Connection variable created...');
+
+connection.connect();
+
+console.log('Connection to MySQL database has been made...');
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
 	return res.sendfile('public/index.html');
 };
+
+/*
+ * GET admin page 
+ */
+
+ exports.admin = function(req, res){
+ 	return res.sendfile('views/admin.jade');
+ }
