01f70e7b557238f1a951198eeefa3cf815183bf2
Andy Heaton <heatonandy@gmail.com>
2014-02-17 14:30:40 -0700

    unimportant changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  * Module dependencies.
  */
 
-var express = require('express'); //hey
+var express = require('express'); 
 var routes 	= require('./routes');
 var user 	= require('./routes/user');
 var http 	= require('http');
