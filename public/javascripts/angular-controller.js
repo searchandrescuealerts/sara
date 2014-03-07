fb5cf10510e49213469f188e999a06fefb34bdd5
l3mm0n <s.l3mm0n@gmail.com>
2014-03-06 22:24:24 -0700

    added new images, started angular on sendMessage page

--- public/javascripts/angular-controller.js
+++ public/javascripts/angular-controller.js
@@ -6,19 +6,19 @@ campaignApp.controller('campaignListCtrl', function ($scope) {
 			"campaignID": "1",
 			"name": "Wendy Darling",
 			"missingSince": "14 Jan 2014",
-			"image": "/images/admin/wendy.jpg"
+			"image": "/images/campaignImages/wendy.jpg"
 		},
 		{
 			"campaignID": "2",
 			"name": "Hansel and Gretel",
 			"missingSince": "8 Feb 2014",
-			"image": "/images/admin/hanselandgretel.jpg"
+			"image": "/images/campaignImages/hanselandgretel.jpg"
 		},
 		{
 			"campaignID": "3",
 			"name": "Belle",
 			"missingSince": "1 March 2014",
-			"image": "/images/admin/belle.jpg"
+			"image": "/images/campaignImages/belle.jpg"
 		}
 	]
 })
