e2866df22c149646d659939c0854f00855942d80
Andy Heaton <heatonandy@gmail.com>
2014-04-03 22:01:01 -0600

    messing with twilio, adding some comments

--- app/controllers/campaign.js
+++ app/controllers/campaign.js
@@ -20,12 +20,12 @@ exports.getAllOpen = function(req, res){
 };
 exports.sendMessage = function(req, res){
 	client.sendMessage({
-		// to: req.body.to,
-		// from: req.body.from,
-		// body: req.body.message
-		to: '+13852045557',
-		from: config.twilio.phone,
-		body: 'what is up home dawg g nation?'
+		to: req.body.to,
+		from: req.body.from,
+		body: req.body.message
+		// to: '303-478-7951',
+		// from: config.twilio.phone,
+		// body: 'Consider the full power of Twilio to be unleashed!!'
 	}, function(err, responseData) {
 		if (!err) {  // "err" is an error received during the request, if any
 
@@ -33,11 +33,13 @@ exports.sendMessage = function(req, res){
         // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
         // http://www.twilio.com/docs/api/rest/sending-sms#example-1
         console.log('This is inside the function after campaign.sendMessage was called. BANGORANG');
-        console.log(responseData.from); // outputs "+14506667788"
-        console.log(responseData.body); // outputs "word to your mother."
+        console.log('Text sent to this number: ' + responseData.to)
+        console.log('Text sent from this number: ' + responseData.from); // outputs "+14506667788"
+        console.log('Body of the text: ' + responseData.body); // outputs "word to your mother."
 
 		}
 	});
+	res.send('Hello!');
 };
 exports.getAllMessages = function(req, res){
 
