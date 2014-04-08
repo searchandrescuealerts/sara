5da31f8418a435286e34d1f00697bb9c1cd634b3
Andy Heaton <heatonandy@gmail.com>
2014-04-07 19:03:17 -0600

    user.js: search_radius_preference now saved in the database (should work for multiple search radius preferences also) myaccount.html: minor commenting

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,6 @@
 var passport = require('../../config/passport');
 var db       = require('../../config/sequelize');
+var util 		= require('util');
 
 // login. 
 // If successful, go to the myaccount page. 
@@ -12,11 +13,11 @@ var db       = require('../../config/sequelize');
 // 		})(req, res, next);
 // };
 
-//Create a new user
+// app.post('/api/v1/campaign') -- Create a new user
 exports.create = function(req, res){
-	console.log('user reqest body: \n ' + req.body.email + " " + req.body.password + " " + req.body.phone + 
-		'\n ' + req.body.locations);
 
+	// Building a user creates a non-persistant object that has not been saved to the db yet.
+	// we build rater than create a user here because the password must be salted and hashed before saving to db.
 	var user = db.User.build(req.body);
 	user.salt = user.makeSalt();
 	user.password = user.encryptPassword(req.body.password, user.salt);
@@ -24,28 +25,49 @@ exports.create = function(req, res){
 		'\n   email: ' + user.email + 
 		'\n   phone: ' + user.phone +
 		'\n   hashed password: ' + user.password + ' }');
-	
+
+	// save the  user object that was just built to the database
 	user.save().success(function(){
+		console.log('User ' + user.email + ' was saved in the database');
 		res.userID = user.id;
-		console.log('in user.js: res.userID: ' + res.userID);
-		console.log('in user.js: user.id: ' + user.id);
-		console.log('in user.js, right before res.send(user), this is the user: ' + user);
 		res.send(user); 
 	}).error(function(err){
 		res.sendfile('public/signup.html');
 	});
+
+	// Loop through all of the locations!
+	for (var i = 0; i < req.body.locations.length; i++){
+ 
+	// We can do a MODEL.create here because there is no further manipulation of fields
+	// before saving the object to the database
+		db.Search_Radius_Preference
+				.create({ 
+					radius: req.body.locations[i].radius, 
+					lat: req.body.locations[i].lat, 
+					lon: req.body.locations[i].lng, 
+					USERId: user.id 
+				}).success(function(srp) {
+					console.log('new search radius preference created for ' 
+						+ user.email + ': \n' + 
+						'lat: ' + srp.lat + ': \n' +
+						'lon: ' + srp.lon + ': \n' +
+						'radius: ' + srp.radius);
+					srp.setUser(user);
+				});
+	}
 };
 
-// PUT user approved to be first responder
+// app.put ('/api/v1/user/:id/approve') -- user approved to be first responder
 exports.approve = function(req, res){
 	
 };
 
-// PUT user apply to be first responder
+// app.put ('/api/v1/user/:id/apply') -- user apply to be first responder
 exports.apply = function(req, res){
 
 };  
 
+// app.get ('/api/v1/user/:id') -- get and return a user
 exports.getUser = function(req, res){
 	//get the ID from the URL
 	var parsedURL = req.url.split("/");
